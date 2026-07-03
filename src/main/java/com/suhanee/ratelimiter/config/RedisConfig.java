0d229cb5a52b52d0c103a3f1574bad6ade131a10
suhanee11 <suhanee03work@gmail.com>
2026-07-03 10:21:27 +0530

    updation in redis config for the deployment

--- src/main/java/com/suhanee/ratelimiter/config/RedisConfig.java
+++ src/main/java/com/suhanee/ratelimiter/config/RedisConfig.java
@@ -1,9 +1,11 @@
 package com.suhanee.ratelimiter.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
@@ -12,9 +14,22 @@ import java.util.List;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.password:}")
+    private String redisPassword;
+
     @Bean
     public RedisConnectionFactory redisConnesctionFactory(){
-    return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration config=new RedisStandaloneConfiguration(redisHost,redisPort);
+        if(redisPassword!=null && !redisPassword.isEmpty()){
+            config.setPassword(redisPassword);
+        }
+    return new LettuceConnectionFactory(config);
 }
 
 @Bean
